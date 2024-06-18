7b18f9ef34ebfc59daa4a6c4173369da3c7bc61c
Y <jydev@yahoo.com>
2024-06-18 22:08:22 +0800

    b

--- app/t/page.js
+++ app/t/page.js
@@ -1,3 +1,10 @@
+export const dynamic = "force-dynamic";
 export default function Page() {
-  return <div>Test page {process.env.tt} abc</div>;
+  let t3 = process.env.ttt;
+  console.log({ t3 });
+  return (
+    <div>
+      Test page: tt:{process.env.tt} ttt:{t3} end
+    </div>
+  );
 }
